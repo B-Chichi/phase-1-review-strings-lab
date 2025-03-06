3b1ff9d17b73c34c490fdf7c350657de9390a556
Brian <brian2000bett@gmail.com>
2025-03-06 22:14:14 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+const currentUser = `Brian Bett`;
+
+console.log(currentUser)
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+console.log(welcomeMessage)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
+console.log(shortGreeting);
